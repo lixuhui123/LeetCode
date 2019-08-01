6807b743e1d6c415a7d8f41c806afd9be5132394
lixuhui123 <2871726754@qq.com>
2019-08-01 22:42:26 +0800

    一些更新

--- LeetCode/LeetCode/ListNode.cpp
+++ LeetCode/LeetCode/ListNode.cpp
@@ -1,100 +1,100 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-typedef int SLTDataType;
-typedef struct SListNode
-{
-	SLTDataType _data;
-	struct SListNode* _next;
-}SListNode;
-typedef struct SList
-{
-	SListNode* _head;
-}SList;
-void SListInit(SList* plist)
-{
-	plist->_head = NULL;
-
-}
-void SListPushFront(SList* plist, SLTDataType x)
-{
-	//链表头压，得到指向链表头结点的指针；
-	SListNode *cur = (SListNode *)malloc(sizeof(SListNode));//给链表结点（结构体）开辟空间
-	cur->_data = x;//赋值
-	cur->_next = plist->_head;//将这个结点的指针指向NULL
-	plist->_head = cur;//将结点此时的地址复制个头指针，让它记住
-	//所以先进入的会后移，和进栈一样，先进后出
-}
-void SListPrint(SList* plist)
-{
-	assert(plist);
-
-	SListNode* cur;
-	for (cur = plist->_head; cur; cur = cur->_next)
-	{
-		printf("%d->", cur->_data);
-	}
-	printf("NULL\n");
-}
-void SListreverse(SList* plist)
-{
-
-	SListNode* tmp = plist->_head->_next;
-	SListNode* cur = plist->_head;
-	while (tmp)
-	{
-		cur->_next = tmp->_next;
-		tmp->_next = plist->_head;
-		plist->_head = tmp;
-		tmp = cur->_next;
-
-	}
-	 
-}
-int main2()
-{
-	/*输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
-		输出：7 -> 0 -> 8
-		原因：342 + 465 = 807*/
-	SList  plist;
-	SListInit(&plist);
-	SListPushFront(&plist, 3);
-	SListPushFront(&plist, 4);
-	SListPushFront(&plist, 2);
-	SListreverse(&plist);
-	SListPrint(&plist);
-	 
-	 
-		 
-	SList plist1;
-	SListInit(&plist1);
-	SListPushFront(&plist1, 4);
-	SListPushFront(&plist1, 6);
-	SListPushFront(&plist1, 5);
-	SListreverse(&plist1);
-	SListPrint(&plist1); 
-	
-	SListNode *tmp = (&plist)->_head;
-	SListNode *tmp1 = (&plist1)->_head;
-		
-	int bai = (tmp->_data) * 100;
-	int bai1 = (tmp1->_data) * 100;
-	int shi = (tmp->_next->_data) * 10;
-	int shi1 = (tmp1->_next->_data) * 10;
-	int ge = (tmp->_next->_next->_data) * 1;
-	int ge1 = (tmp1->_next->_next->_data) * 1;
-	int shu = bai + shi + ge;
-	int shu1 = bai1 + shi1 + ge1;
-	//printf("%d  %d\n", shu, shu1);
-	int add = shu + shu1;
-
-	SList plist2;
-	SListInit(&plist2);
-	SListPushFront(&plist2, add/100);
-	SListPushFront(&plist2, add/10%10);
-	SListPushFront(&plist2, add%10);
-	SListPrint(&plist2);
-
-    system("pause");
-    return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//typedef int SLTDataType;
+//typedef struct SListNode
+//{
+//	SLTDataType _data;
+//	struct SListNode* _next;
+//}SListNode;
+//typedef struct SList
+//{
+//	SListNode* _head;
+//}SList;
+//void SListInit(SList* plist)
+//{
+//	plist->_head = NULL;
+//
+//}
+//void SListPushFront(SList* plist, SLTDataType x)
+//{
+//	//链表头压，得到指向链表头结点的指针；
+//	SListNode *cur = (SListNode *)malloc(sizeof(SListNode));//给链表结点（结构体）开辟空间
+//	cur->_data = x;//赋值
+//	cur->_next = plist->_head;//将这个结点的指针指向NULL
+//	plist->_head = cur;//将结点此时的地址复制个头指针，让它记住
+//	//所以先进入的会后移，和进栈一样，先进后出
+//}
+//void SListPrint(SList* plist)
+//{
+//	assert(plist);
+//
+//	SListNode* cur;
+//	for (cur = plist->_head; cur; cur = cur->_next)
+//	{
+//		printf("%d->", cur->_data);
+//	}
+//	printf("NULL\n");
+//}
+//void SListreverse(SList* plist)
+//{
+//
+//	SListNode* tmp = plist->_head->_next;
+//	SListNode* cur = plist->_head;
+//	while (tmp)
+//	{
+//		cur->_next = tmp->_next;
+//		tmp->_next = plist->_head;
+//		plist->_head = tmp;
+//		tmp = cur->_next;
+//
+//	}
+//	 
+//}
+//int main2()
+//{
+//	/*输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//		输出：7 -> 0 -> 8
+//		原因：342 + 465 = 807*/
+//	SList  plist;
+//	SListInit(&plist);
+//	SListPushFront(&plist, 3);
+//	SListPushFront(&plist, 4);
+//	SListPushFront(&plist, 2);
+//	SListreverse(&plist);
+//	SListPrint(&plist);
+//	 
+//	 
+//		 
+//	SList plist1;
+//	SListInit(&plist1);
+//	SListPushFront(&plist1, 4);
+//	SListPushFront(&plist1, 6);
+//	SListPushFront(&plist1, 5);
+//	SListreverse(&plist1);
+//	SListPrint(&plist1); 
+//	
+//	SListNode *tmp = (&plist)->_head;
+//	SListNode *tmp1 = (&plist1)->_head;
+//		
+//	int bai = (tmp->_data) * 100;
+//	int bai1 = (tmp1->_data) * 100;
+//	int shi = (tmp->_next->_data) * 10;
+//	int shi1 = (tmp1->_next->_data) * 10;
+//	int ge = (tmp->_next->_next->_data) * 1;
+//	int ge1 = (tmp1->_next->_next->_data) * 1;
+//	int shu = bai + shi + ge;
+//	int shu1 = bai1 + shi1 + ge1;
+//	//printf("%d  %d\n", shu, shu1);
+//	int add = shu + shu1;
+//
+//	SList plist2;
+//	SListInit(&plist2);
+//	SListPushFront(&plist2, add/100);
+//	SListPushFront(&plist2, add/10%10);
+//	SListPushFront(&plist2, add%10);
+//	SListPrint(&plist2);
+//
+//    system("pause");
+//    return 0;
+//}
--- LeetCode/LeetCode/two_nums_add.cpp
+++ LeetCode/LeetCode/two_nums_add.cpp
@@ -1,41 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-//给定一个整数数组 nums 和一个目标值 target，
-//请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
-	//numsSize = sizeof(nums)/sizeof(int);1,致命错误
-	printf("%d\n", numsSize);
-	int i = 0;
-	int j = 0;
-	for (i=0;i<numsSize-1;i++)
-	{
-		for (j=i+1;j<numsSize;j++)
-		{
-		    if (target == *(nums + i) + *(nums + j))
-            {
-				returnSize[0] = i;
-				returnSize[1] = j;
-				return returnSize;
-            }
-			else
-			{
-				continue;
-			}
-        
-		}
-	}
-	return NULL;
-}
-int main1()
-{
-	int nums[] = { 2,7,11,15 };
-	int target = 9;
-	int numsSize = 0;
-	int returnSize[2] = { 0 };
-	numsSize = sizeof(nums) / sizeof(int);
-	twoSum(nums, numsSize, target, returnSize);
-	printf("%d %d\n", returnSize[0], returnSize[1]);
-    system("pause");
-    return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+////给定一个整数数组 nums 和一个目标值 target，
+////请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+//{
+//	//numsSize = sizeof(nums)/sizeof(int);1,致命错误
+//	printf("%d\n", numsSize);
+//	int i = 0;
+//	int j = 0;
+//	for (i=0;i<numsSize-1;i++)
+//	{
+//		for (j=i+1;j<numsSize;j++)
+//		{
+//		    if (target == *(nums + i) + *(nums + j))
+//            {
+//				returnSize[0] = i;
+//				returnSize[1] = j;
+//				return returnSize;
+//            }
+//			else
+//			{
+//				continue;
+//			}
+//        
+//		}
+//	}
+//	return NULL;
+//}
+//int main1()
+//{
+//	int nums[] = { 2,7,11,15 };
+//	int target = 9;
+//	int numsSize = 0;
+//	int returnSize[2] = { 0 };
+//	numsSize = sizeof(nums) / sizeof(int);
+//	twoSum(nums, numsSize, target, returnSize);
+//	printf("%d %d\n", returnSize[0], returnSize[1]);
+//    system("pause");
+//    return 0;
+//}
